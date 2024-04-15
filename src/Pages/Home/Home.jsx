ad0d1db7dedc4224e7512894bf1729a1de102137
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-04-15 13:53:35 +0600

    customer review section done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../Components/Banner/Banner";
+import Review from "../../Components/Review/Review";
 
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Review></Review>
     </div>
   );
 };
